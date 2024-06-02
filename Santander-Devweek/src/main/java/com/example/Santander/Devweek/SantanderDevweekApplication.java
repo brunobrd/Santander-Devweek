5e274ee493c11d312edc211557332b39b092a99a
brunobrd <brunobrdk@outlook.com>
2024-06-02 14:25:21 -0300

    java Api

--- Santander-Devweek/src/main/java/com/example/Santander/Devweek/SantanderDevweekApplication.java
+++ Santander-Devweek/src/main/java/com/example/Santander/Devweek/SantanderDevweekApplication.java
@@ -1,12 +1,16 @@
 package com.example.Santander.Devweek;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 
 @SpringBootApplication
 public class SantanderDevweekApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SantanderDevweekApplication.class, args);
 	}
 
