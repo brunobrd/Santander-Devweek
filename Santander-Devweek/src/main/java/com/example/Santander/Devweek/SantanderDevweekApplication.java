76680a55a42c556b66a0731dc2abf272e21d8ad6
brunobrd <brunobrdk@outlook.com>
2024-06-02 14:25:21 -0300

    Initial commit

---
+++ Santander-Devweek/src/main/java/com/example/Santander/Devweek/SantanderDevweekApplication.java
@@ -0,0 +1,13 @@
+package com.example.Santander.Devweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevweekApplication.class, args);
+	}
+
+}
---
+++ Santander-Devweek/src/test/java/com/example/Santander/Devweek/SantanderDevweekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Santander.Devweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevweekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
